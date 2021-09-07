9bcd547642825f222649ff1986bd6be0f4d0dd3e
tuyendev <tuyendev@gmail.com>
2021-09-07 18:01:09 +0700

    add microservices

---
+++ ordermanagement/src/test/java/com/tuyen/demo/ordermanagement/OrdermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyen.demo.ordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
